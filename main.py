e8f27338f589e364bacc718749d45c3f4af8223c
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-10-13 16:35:13 -0700

    Initial commint.

---
+++ main.py
@@ -0,0 +1,134 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2020-10-13, File Created.
+# Python and Pygame implementation of classic Snake game.
+# --------------------------------------------------------------------------- #
+import pygame
+import sys
+import random
+
+
+class Snake(object):
+    """Snake object."""
+
+    def __init__(self, ):
+        """Constructor for snake"""
+        self.lenght = 1
+        self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.color = (17, 24, 47)
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def turn(self, point):
+        if self.lenght > 1 and (point[0] * -1, point[1] * -1) == self.direction:
+            return
+        else:
+            self.direction = point
+
+    def move(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x * GRIDSIZE)) % SCREEN_WIDTH), (cur[1] + (y * GRIDSIZE)) % SCREEN_WIDTH)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.lenght:
+                self.positions.pop()
+
+    def reset(self):
+        self.lenght = 1
+        self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+    def draw(self, surface):
+        for p in self.positions:
+            r = pygame.Rect((p[0], p[1]), (GRIDSIZE, GRIDSIZE))
+            pygame.draw.rect(surface, self.color, r)
+            pygame.draw.rect(surface, (93, 216, 228), r, 1)
+
+    def handle_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_UP:
+                    self.turn(UP)
+                elif event.type == pygame.K_DOWN:
+                    self.turn(DOWN)
+                elif event.type == pygame.K_LEFT:
+                    self.turn(LEFT)
+                elif event.type == pygame.K_RIGHT:
+                    self.turn(RIGHT)
+
+
+class Food(object):
+    """Food object."""
+
+    def __init__(self, ):
+        """Constructor for food"""
+        self.position = (0, 0)
+        self.color = (223, 163, 49)
+        self.randomize_position()
+
+    def randomize_position(self):
+        self.position = (random.randint(0, GRID_WIDTH-1) * GRIDSIZE, random.randint(0, GRID_HEIGTH-1) * )
+
+    def draw(self, surface):
+        pass
+
+
+def draw_grid(surface):
+    for y in range(0, int(GRID_HEIGTH)):
+        for x in range(0, int(GRID_WIDTH)):
+            if (x + y) % 2 == 0:
+                r = pygame.Rect((x * GRIDSIZE, y * GRIDSIZE),
+                                (GRIDSIZE, GRIDSIZE))
+                pygame.draw.rect(surface, (93, 216, 228), r)
+            else:
+                rr = pygame.Rect((x * GRIDSIZE, y * GRIDSIZE),
+                                 (GRIDSIZE, GRIDSIZE))
+                pygame.draw.rect(surface, (84, 194, 205), rr)
+
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 480
+
+GRIDSIZE = 20
+GRID_WIDTH = SCREEN_HEIGHT / GRIDSIZE
+GRID_HEIGTH = SCREEN_HEIGHT / GRIDSIZE
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+
+def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+    draw_grid(surface)
+
+    snake = Snake()
+    food = Food()
+
+    score = 0
+
+    while True:
+        clock.tick(10)
+
+        # handle events
+
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
+
+
+
+main()
